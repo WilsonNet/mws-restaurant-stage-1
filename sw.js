0fdbbc4d5c586024f3cfb608ab4492cd0b377bdd
Wilson Neto <wilsonpsn@hotmail.com>
2019-02-02 21:26:59 -0200

    Sw is caching

--- sw.js
+++ sw.js
@@ -1,4 +1,19 @@
+self.addEventListener('install', evt=>{
+const urlsToCach = [
+    '/',
+    '/css/styles.css',
+    '/js/',
+    '/js/main.js',
+    '/js/restaurant_info.js',
+    'js/dbhelper.js'
+];
+
+    evt.waitUntil(
+        caches.open('v1').then(cache=> cache.addAll(urlsToCach))
+    );
+
+});
+
 
 self.addEventListener('fetch', evt=> {
-    console.log(" instalado");
 });
