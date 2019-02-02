6da993569d26860a4370264f1d4a3a3b1cd5420a
Wilson Neto <wilsonpsn@hotmail.com>
2019-02-02 21:52:08 -0200

    Utilizing cache

--- sw.js
+++ sw.js
@@ -1,19 +1,27 @@
-self.addEventListener('install', evt=>{
-const urlsToCach = [
-    '/',
-    '/css/styles.css',
-    '/js/',
-    '/js/main.js',
-    '/js/restaurant_info.js',
-    'js/dbhelper.js'
-];
+self.addEventListener('install', evt => {
+    const urlsToCach = [
+        '/',
+        'css/styles.css',
+        'js/',
+        'js/main.js',
+        'js/restaurant_info.js',
+        'js/dbhelper.js',
+        'https://unpkg.com/leaflet@1.3.1/dist/leaflet.css',
+        '/restaurant.html'
+    ];
 
     evt.waitUntil(
-        caches.open('v1').then(cache=> cache.addAll(urlsToCach))
+        caches.open('v1').then(cache => cache.addAll(urlsToCach))
     );
 
 });
 
 
-self.addEventListener('fetch', evt=> {
+self.addEventListener('fetch', evt => {
+    evt.respondWith(
+        caches.match(evt.request)
+            .then(response => response || fetch(evt.request))
+            .catch(err=>console.log(err))
+
+    )
 });
